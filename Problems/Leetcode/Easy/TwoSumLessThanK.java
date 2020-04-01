6bdf8e8d6a5292e930fd41cebb2cd3b367cd8919
Arjun Dass <ad.sam007@gmail.com>
2020-03-31 20:05:40 -0400

    Amazon Easy problems

---
+++ Problems/Leetcode/Easy/TwoSumLessThanK.java
@@ -0,0 +1,19 @@
+import java.util.TreeSet;
+
+class TwoSumLessThanK {
+    public int twoSumLessThanK(int[] A, int K) {
+       TreeSet<Integer> set = new TreeSet<Integer>();
+        int res = -1;
+        for (int a: A) {
+            Integer pre = set.lower(K - a);
+            
+            if (pre != null) {
+                res = Math.max( a+pre, res);
+            }
+            
+            set.add(a);
+        }
+        
+        return res;
+    }
+}
