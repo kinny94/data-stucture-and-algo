c062078c2abda89bb307c22b999bcbbe1d669fe6
Arjun Dass <ad.sam007@gmail.com>
2020-04-01 00:50:24 -0400

    most common word and subtree of another tree

---
+++ Problems/Leetcode/Easy/SubTreeOfAnotherTree.java
@@ -0,0 +1,19 @@
+class SubTreeOfAnotherTree {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (checkSubTree(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    public boolean checkSubTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        
+        if (s.val != t.val) return false;
+        
+        
+        return checkSubTree(s.left, t.left) && checkSubTree(s.right, t.right);
+        
+    }
+    
+}
