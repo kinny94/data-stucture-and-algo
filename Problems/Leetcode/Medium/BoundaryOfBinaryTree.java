b6eb37a2c0b99ee942873d50404b0de60ee3a4e4
Arjun Dass <ad.sam007@gmail.com>
2020-04-03 22:45:33 -0400

    boundary of binary tree

---
+++ Problems/Leetcode/Medium/BoundaryOfBinaryTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class BinaryOfBinaryTree {
+    
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+       List<Integer> res = new ArrayList<Integer>();
+       if (root != null) {
+           res.add(root.val);
+           getBounds(root.left, res, true, false);
+           getBounds(root.right, res, false, true);
+       }
+       return res;
+   }
+   
+      private void getBounds(TreeNode node, List<Integer> res, boolean lb, boolean rb) {
+       if (node == null) return;
+       if (lb) res.add(node.val);
+       if (!lb && !rb && node.left == null && node.right == null) res.add(node.val);
+       getBounds(node.left, res, lb, rb && node.right == null);
+       getBounds(node.right, res, lb && node.left == null, rb);
+       if (rb) res.add(node.val);
+   }
+   
+}
