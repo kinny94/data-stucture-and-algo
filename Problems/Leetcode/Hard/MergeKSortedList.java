19099be82cf4b2a05ed99967f2700479e2efc3f2
kinny <ad.sam007@gmail.com>
2020-04-06 22:19:09 -0400

    Merge K sorted list

---
+++ Problems/Leetcode/Hard/MergeKSortedList.java
@@ -0,0 +1,23 @@
+class MergeKSortedList {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists==null || lists.length==0) return null;
+        
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length, (a,b)-> a.val-b.val);
+        
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+        
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+            
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
+    }
+}
