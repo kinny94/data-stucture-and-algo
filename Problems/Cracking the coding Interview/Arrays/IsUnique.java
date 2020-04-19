449f7808a970ca418fc063849abff419574876e9
kinny <ad.sam007@gmail.com>
2020-04-19 13:49:12 -0400

    is unique

---
+++ Problems/Cracking the coding Interview/Arrays/IsUnique.java
@@ -0,0 +1,25 @@
+class IsUnique {
+    public boolean isUnique(String str) {
+        if (str.length() > 128) {
+            return false;
+        }
+
+        boolean[] char_set = new boolean[128];
+
+        for (int i=0; i<str.length(); i++) {
+            int val = str.charAt(i);
+            if (char_set[val]) {
+                return false;
+            }
+
+            char_set[val] = true;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        IsUnique obj = new IsUnique();
+        System.out.println(obj.isUnique("arjunds"));
+    }
+}
