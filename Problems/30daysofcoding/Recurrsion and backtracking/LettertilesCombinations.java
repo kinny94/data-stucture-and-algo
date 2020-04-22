bab83ce4330b1b43287eeb7124b9a8050f176a2b
kinny <ad.sam007@gmail.com>
2020-04-22 15:44:29 -0400

    letter tiles combinations

---
+++ Problems/30daysofcoding/Recurrsion and backtracking/LettertilesCombinations.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class LettertilesCombinations {
+
+    public int numTilePossibilities(String tiles) {
+        char[] chars = tiles.toCharArray();
+        Arrays.sort(chars);
+        boolean[] visited = new boolean[chars.length];
+        return dfs(chars,visited);
+    }
+    
+    private int dfs(char[] chars, boolean[] visited){
+        int count = 0;
+        for(int i = 0; i < chars.length; i++){
+            if(visited[i]) continue;
+            if(i - 1 >= 0 && chars[i] == chars[i - 1] && !visited[i - 1]) continue;
+            visited[i] = true;
+            count++;
+            count+=dfs(chars, visited);
+            visited[i] = false;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        LettertilesCombinations obj = new LettertilesCombinations();
+        System.out.println(obj.numTilePossibilities("AAB"));
+    }
+
+}
