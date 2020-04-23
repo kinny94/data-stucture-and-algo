2df4036691cae1e80b3c54de6944b6768a63130d
kinny <ad.sam007@gmail.com>
2020-04-23 01:52:08 -0400

    recent counter

---
+++ Problems/30daysofcoding/Stack and Queue/RecentCounter.java
@@ -0,0 +1,13 @@
+class RecentCounter {
+    Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        q.offer(t);
+        while (q.peek() < t - 3000) { q.poll(); }
+        return q.size();
+    }
+}
