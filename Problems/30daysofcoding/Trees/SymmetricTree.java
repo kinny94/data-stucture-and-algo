09a1563454301eb84b1327e23af9eaf96d9ab592
kinny <ad.sam007@gmail.com>
2020-04-25 17:51:28 -0400

    isbsymmetric binary tree

---
+++ Problems/30daysofcoding/Trees/SymmetricTree.java
@@ -0,0 +1,25 @@
+class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.right == null && root.left == null) {
+            return true;
+        }
+
+        return helper(root.left, root.right);
+    }
+
+    public boolean helper(TreeNode left,  TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
