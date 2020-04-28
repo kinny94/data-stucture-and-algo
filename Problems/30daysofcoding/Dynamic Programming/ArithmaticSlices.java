04ec956344bc63c5547107d1e7203edebdf4b699
kinny <ad.sam007@gmail.com>
2020-04-28 02:25:52 -0400

    numbr of arithmetic slices

---
+++ Problems/30daysofcoding/Dynamic Programming/ArithmaticSlices.java
@@ -0,0 +1,25 @@
+class ArithmaticSlices {
+    public int numberOfArithmeticSlices(int[] A) {
+        int n =  A.length;
+        if (n < 3) { 
+            return 0;
+        }
+
+        int[] dp = new int[n];
+        dp[0] = 0;
+        dp[1] = 0;
+        int sum = 0;
+
+        for (int i=2; i<n; i++) {
+            if ((A[i] - A[i-1]) == (A[i-1] - A[i-2])) {
+                dp[i] = dp[i-1] + 1;
+            } else {
+                dp[i] = 0;
+            }
+
+            sum += dp[i];
+        }
+
+        return sum;
+    }
+}
