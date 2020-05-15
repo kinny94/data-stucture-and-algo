da87cb642d41032448c00ba2af3d26d2cd555e66
kinny <ad.sam007@gmail.com>
2020-05-14 22:37:36 -0400

    rearrange in place

--- Educative.io/Data Structures for interviews/Arrays/ReArrangeArray.java
+++ Educative.io/Data Structures for interviews/Arrays/ReArrangeArray.java
@@ -1,27 +1,16 @@
 class CheckReArrange {
     //Re-Arrange Positive and Negative Values of Given Array 
-    public static void reArrange(int[] arr) 
-    {
-        
-        int[] newArray = new int[arr.length];
-        int newArray_index = 0;
-        
-        //Fill newArray with Negative Values first.
-        //Then Fill it with Postive Values.
-        //In the end, insert every element of newArray back into origional arr.
-        for (int i = 0; i < arr.length; i++) {
-            
-            if (arr[i] < 0) newArray[newArray_index++] = arr[i];
-        }
-        
-        for (int i = 0; i < arr.length; i++) {
-            
-            if (arr[i] > 0) newArray[newArray_index++] = arr[i];
-        }
-        
-        for (int j = 0; j < newArray.length; j++) 
-        {
-            arr[j] = newArray[j];
-        }
-    } //end of reArrange()
+    public static void reArrange(int[] arr) {
+        int j = 0; 
+        for (int i = 0; i < arr.length; i++) { 
+            if (arr[i] < 0) {   // if negative number found
+                if (i != j) { 
+                    int temp = arr[i];
+                    arr[i] = arr[j]; // swapping with leftmost positive
+                    arr[j] = temp;
+                }
+                j++; 
+            } 
+        } 
+    }
 }
