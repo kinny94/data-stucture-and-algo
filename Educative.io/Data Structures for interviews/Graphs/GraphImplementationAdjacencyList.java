27af0fffc067c7bef240f5c9cd7ae5ce934cbe5a
Arjun Dass <ad.sam007@gmail.com>
2020-05-16 19:20:11 -0400

    min stack and graph implementation

---
+++ Educative.io/Data Structures for interviews/Graphs/GraphImplementationAdjacencyList.java
@@ -0,0 +1,165 @@
+class DoublyLinkedList<T> {
+
+    //Node inner class for DLL
+    public class Node {
+        public T data;
+        public Node nextNode;
+        public Node prevNode;
+    }
+
+    public Node headNode;
+    public Node tailNode;
+    public int size;
+
+    public DoublyLinkedList() {
+        this.headNode = null;
+        this.tailNode = null;
+    }
+
+    public boolean isEmpty() {
+        if (headNode == null && tailNode == null)
+            return true;
+        return false;
+    }
+
+    public Node getHeadNode() {
+        return headNode;
+    }
+
+    public Node getTailNode() {
+        return tailNode;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void insertAtHead(T data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.nextNode = this.headNode; //Linking newNode to head's nextNode
+        newNode.prevNode = null;
+        if (headNode != null)
+            headNode.prevNode = newNode;
+        else
+            tailNode = newNode;
+        this.headNode = newNode;
+        size++;
+    }
+
+    public void insertAtEnd(T data) {
+        if(isEmpty()) {
+            insertAtHead(data);
+            return;
+        }
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.nextNode = null;
+        newNode.prevNode = tailNode;
+        tailNode.nextNode = newNode;
+        tailNode = newNode;
+        size++;
+    }
+
+    public void printList() {
+        if (isEmpty()) {
+            System.out.println("List is Empty!");
+            return;
+        }
+
+        Node temp = headNode;
+        System.out.print("List : null <- ");
+
+        while (temp.nextNode != null) {
+            System.out.print(temp.data.toString() + " <-> ");
+            temp = temp.nextNode;
+        }
+
+        System.out.println(temp.data.toString() + " -> null");
+    }
+
+    public void printListReverse() {
+        if (isEmpty()) {
+            System.out.println("List is Empty!");
+        }
+
+        Node temp = tailNode;
+        System.out.print("List : null <- ");
+
+        while (temp.prevNode != null) {
+            System.out.print(temp.data.toString() + " <-> ");
+            temp = temp.prevNode;
+        }
+
+        System.out.println(temp.data.toString() + " -> null");
+    }
+
+    public void deleteAtHead() {
+        if (isEmpty())
+            return;
+
+        headNode = headNode.nextNode;
+        if (headNode == null)
+            tailNode = null;
+        else
+            headNode.prevNode = null;
+        size--;
+    }
+
+    public void deleteAtTail() {
+        if (isEmpty())
+            return;
+        tailNode = tailNode.prevNode;
+        if (tailNode == null)
+            headNode = null;
+        else
+            tailNode.nextNode = null;
+        size--;
+    }
+}
+
+class GraphImplementationAdjacencyList {
+    int vertices; //Total number of vertices in graph
+    DoublyLinkedList<Integer> adjacencyList[]; //An array of linked lists to store adjacent vertices.
+    
+    @SuppressWarnings("unchecked")
+    public GraphImplementationAdjacencyList(int vertices) {
+        this.vertices = vertices;
+        adjacencyList = new DoublyLinkedList[vertices];
+
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList[i] = new DoublyLinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination){
+        if (source < vertices && destination < vertices){
+        this.adjacencyList[source].insertAtEnd(destination);
+
+        //for undirected graph uncomment the line below
+        //this.adjacencyList[destination].insertAtEnd(source);
+        }
+    }
+    public void printGraph()
+    {
+        System.out.println(">>Adjacency List of Directed Graph<<");
+        for (int i = 0; i < vertices; i++)
+        {
+            if(adjacencyList[i]!=null){
+                System.out.print("|" + i + "| => ");
+
+                DoublyLinkedList<Integer>.Node temp = adjacencyList[i].getHeadNode();
+                while (temp != null)
+                {
+                    System.out.print("[" + temp.data + "] -> ");
+                    temp = temp.nextNode;
+                }
+                System.out.println("null");
+            }
+            else{
+
+                System.out.println("|" + i + "| => "+ "null");
+            }
+        }
+    }
+}
