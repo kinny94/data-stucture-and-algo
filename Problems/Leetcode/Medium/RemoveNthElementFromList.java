2ade5d492f0d093355b4dff7c81fa935bf4be541
kinny <ad.sam007@gmail.com>
2020-05-16 12:54:06 -0400

    remove nth element from the list

---
+++ Problems/Leetcode/Medium/RemoveNthElementFromList.java
@@ -0,0 +1,23 @@
+class RemoveNthElementFromList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode p1 = head;
+        ListNode p2 = head;
+
+        for (int i=0; i<n; i++) {
+            if (p1.next == null) {
+                return head.next;
+            }
+
+            p1 = p1.next;
+        }
+
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        p2.next = p2.next.next; 
+        return head;
+    }
+}
