291bcac124b8caa01ab548b389fda0a448c1b8c5
kinny <ad.sam007@gmail.com>
2020-05-20 14:34:34 -0400

    combindational sum

---
+++ Problems/Leetcode/Medium/CombinationSum.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<List<Integer>> results = new ArrayList<>();
+
+        if (candidates == null || candidates.length == 0) {
+            return results;
+        }
+
+        Arrays.sort(candidates);
+        createCombinations(results, new ArrayList<Integer>(), 0, candidates, target);
+        return results;
+    } 
+
+    public void createCombinations(List<List<Integer>> results, List<Integer> current, int index, int[] candidates, int target) {
+        
+        if (target == 0) {
+            results.add(new ArrayList<>(current));
+        }
+
+        for (int i=index; i<candidates.length; i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+
+            current.add(candidates[i]);
+            createCombinations(results, current, i, candidates, target - candidates[i]);
+            current.remove(current.size() - 1);
+        }
+    }
+}
