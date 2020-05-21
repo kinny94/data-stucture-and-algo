06755f534fd2d090008170fc0b517e058a750c54
kinny <ad.sam007@gmail.com>
2020-05-21 14:50:38 -0400

    symmetrixtree

---
+++ Problems/Leetcode/Easy/SymmetricTree.java
@@ -0,0 +1,26 @@
+class SymmetricTree {
+    
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSame(root.left, root.right);
+    }
+
+    private boolean isSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isSame(left.left, right.right) && isSame(left.right, right.left);
+    }
+}
