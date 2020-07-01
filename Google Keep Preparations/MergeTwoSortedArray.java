a092ec7eb812f92fe4e9ffee05b93b00440f7c5e
kinny <ad.sam007@gmail.com>
2020-07-01 16:15:04 -0400

    merge two sorted array

---
+++ Google Keep Preparations/MergeTwoSortedArray.java
@@ -0,0 +1,26 @@
+class MergeTwoSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int count = m + n - 1;
+        m--;
+        n--;
+
+        while (m>=0 && n>=0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[count] = nums1[m];
+                count--;
+                m--;
+            } else {
+                nums1[count] = nums2[n];
+                count--;
+                n--;
+            }
+        }
+
+        while (n >=0) {
+            nums1[count] = nums2[n];
+            count--;
+            n--;
+        }
+    }    
+}
