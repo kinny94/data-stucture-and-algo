8f1ec0865a69f71bfef091f890558e7e8712ddae
kinny <ad.sam007@gmail.com>
2020-07-02 13:17:17 -0400

    Linked list cycle

---
+++ Google Keep Preparations/Linked List/LinkedListCycle.java
@@ -0,0 +1,27 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
