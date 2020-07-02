c83952d745a4014e6ac06847c219531c49b421df
kinny <ad.sam007@gmail.com>
2020-07-02 13:30:10 -0400

    copy list with random pointer

---
+++ Google Keep Preparations/Linked List/CopyListWithRandomPointer.java
@@ -0,0 +1,32 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+
+class CopyListWithRandomPointer {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Map<Node, Node> map = new HashMap<>();
+        Node temp = head;
+
+        while (temp != null) {
+            map.put(temp, new Node(temp.val));
+            temp = temp.next;
+        }
+
+        temp = head;
+
+        while (temp != null) {
+            map.get(temp).next = map.get(temp.next);
+            map.get(temp).random = map.get(temp.random);
+            temp = temp.next;
+        }
+
+        return map.get(head);
+    }
+}
