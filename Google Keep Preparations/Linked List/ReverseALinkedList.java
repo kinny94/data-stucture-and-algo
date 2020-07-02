a1cc65539338556c9eb8f451608f7f496066f0b5
kinny <ad.sam007@gmail.com>
2020-07-02 13:24:52 -0400

    reverse a linked list

---
+++ Google Keep Preparations/Linked List/ReverseALinkedList.java
@@ -0,0 +1,20 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+
+class ReverseALinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }    
+
+        return newHead;
+    }
+}
