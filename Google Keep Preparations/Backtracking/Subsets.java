5e99b6b650d015848f42e7a82c3e934ce64d354f
kinny <ad.sam007@gmail.com>
2020-07-07 16:18:42 -0400

    subsets and word search

---
+++ Google Keep Preparations/Backtracking/Subsets.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class SubSets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        if (nums == null || nums.length == 0) {
+            return results;
+        }
+
+        createSubsets(results, new ArrayList<>(), 0, nums);
+        return results;
+    }
+
+    private void createSubsets(List<List<Integer>> results, List<Integer> currentList, int index, int[] nums) {
+        results.add(new ArrayList<>(currentList));
+
+        for (int i=index; i<nums.length; i++) {
+            currentList.add(nums[i]);
+            createSubsets(results, currentList, i + 1, nums);
+            currentList.remove(currentList.size() - 1);
+        }
+    }
+}
