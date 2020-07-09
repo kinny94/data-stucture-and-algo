c9d44aea70960e301ecfca420c472f6371857fc8
kinny <ad.sam007@gmail.com>
2020-07-09 16:24:33 -0400

    validate bst

---
+++ Google Keep Preparations/Trees/ValidateBST.java
@@ -0,0 +1,19 @@
+class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode node, Integer max, Integer min) {
+        if (node == null) {
+            return true;
+        } else if (max != null && node.val >= max || min != null && node.val <= min) {
+            return false;
+        } else {
+            return helper(node.left, node.val, min) && helper(node.right, max, node.val);
+        }
+    }
+}
