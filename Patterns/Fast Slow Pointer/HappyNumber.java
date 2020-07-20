a895afb428aa17b3a1db22ac7854fd0cb1affb90
kinny <ad.sam007@gmail.com>
2020-07-20 13:55:33 -0400

    Middle of a linked lsit and happy number

---
+++ Patterns/Fast Slow Pointer/HappyNumber.java
@@ -0,0 +1,32 @@
+class HappyNumber {
+    public static boolean find(int num) {
+        int slow = num;
+        int fast = num;
+
+        do {
+            slow = findSquareSum(slow);
+            fast = findSquareSum(findSquareSum(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }    
+
+    private static int findSquareSum(int num) {
+        int sum = 0;
+        int digit;
+
+        while (num > 0) {
+            digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HappyNumber.find(23));
+        System.out.println(HappyNumber.find(12));
+    }
+    
+}
