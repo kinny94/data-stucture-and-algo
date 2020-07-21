d2ef858f5be24ec768762ed195d10596cbc037a2
kinny <ad.sam007@gmail.com>
2020-07-21 14:08:25 -0400

    maximum cpu load

---
+++ Patterns/Merge Intervals/MaximumCPULoad.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Job {
+    int start;
+    int end;
+    int cpuLoad;
+
+    public Job(int start, int end, int cpuLoad) {
+        this.start = start;
+        this.end = end;
+        this.cpuLoad = cpuLoad;
+    }
+}
+
+class MaximumCPULoad {
+    public static int findMaxCPULoad(List<Job> jobs) {
+        Collections.sort(jobs, (a, b) -> Integer.compare(a.start, b.start));
+
+        int maxCPULoad = 0;
+        int currentCPULoad = 0;
+        PriorityQueue<Job> minHeap = new PriorityQueue<>(jobs.size(), (a,b) -> Integer.compare(a.end, b.end));
+        
+        for (Job job: jobs) {
+            while (!minHeap.isEmpty() && job.start > minHeap.peek().end) {
+                currentCPULoad -= minHeap.poll().cpuLoad;
+            }
+
+            minHeap.offer(job);
+            currentCPULoad += job.cpuLoad;
+            maxCPULoad = Math.max(maxCPULoad, currentCPULoad);
+        }
+
+        return maxCPULoad;
+    }
+
+    public static void main(String[] args) {
+        List<Job> input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 3), new Job(2, 5, 4), new Job(7, 9, 6)));
+        System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
+
+        input = new ArrayList<Job>(Arrays.asList(new Job(6, 7, 10), new Job(2, 4, 11), new Job(8, 12, 15)));
+        System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
+
+        input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 2), new Job(2, 4, 1), new Job(3, 6, 5)));
+        System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
+    }
+}
