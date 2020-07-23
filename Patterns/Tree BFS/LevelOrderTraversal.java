1b6a6560f4346f8b30544ca8ca58c6e530496434
kinny <ad.sam007@gmail.com>
2020-07-23 01:34:55 -0400

    level order teraversal

---
+++ Patterns/Tree BFS/LevelOrderTraversal.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class LevelOrderTraversal {
+
+    public static List<List<Integer>> traverse(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i=0; i<levelSize; i++) {
+                
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<List<Integer>> result = LevelOrderTraversal.traverse(root);
+        System.out.println("Level order traversal: " + result);
+    }
+}
