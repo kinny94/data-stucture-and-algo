48b72e48a63656b45788cbfaa8df46d3ab29b13e
kinny <ad.sam007@gmail.com>
2020-07-24 23:32:58 -0400

    binary search element

---
+++ Patterns/Binary Search/MaximumInBitonicArray.java
@@ -0,0 +1,24 @@
+class MaximumInBitonicArray {
+    public static int findMax(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return arr[start];
+    }   
+    
+    public static void main(String[] args) {
+        System.out.println(MaximumInBitonicArray.findMax(new int[] { 1, 3, 8, 12, 4, 2 }));
+        System.out.println(MaximumInBitonicArray.findMax(new int[] { 3, 8, 3, 1 }));
+        System.out.println(MaximumInBitonicArray.findMax(new int[] { 1, 3, 8, 12 }));
+        System.out.println(MaximumInBitonicArray.findMax(new int[] { 10, 9, 8 }));
+    }
+}
---
+++ Patterns/Binary Search/MinimumDifferenceElement.java
@@ -0,0 +1,39 @@
+class MinimumDifferenceElement {
+
+    public static int searchMinDiffElement(int[] arr, int key) {
+        if (key < arr[0]) {
+            return arr[0];
+        }
+
+        if (key > arr[arr.length - 1]) {
+            return arr[arr.length - 1];
+        }
+
+        int start = 0;
+        int end =arr.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < arr[mid]) {
+                end = mid - 1;
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return arr[mid];
+            }
+        }
+
+        if ((arr[start] - key) < (key - arr[end])) {
+            return arr[start];
+        }
+
+        return arr[end];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MinimumDifferenceElement.searchMinDiffElement(new int[] { 4, 6, 10 }, 7));
+        System.out.println(MinimumDifferenceElement.searchMinDiffElement(new int[] { 4, 6, 10 }, 4));
+        System.out.println(MinimumDifferenceElement.searchMinDiffElement(new int[] { 1, 3, 8, 10, 15 }, 12));
+        System.out.println(MinimumDifferenceElement.searchMinDiffElement(new int[] { 4, 6, 10 }, 17));
+    }    
+}
