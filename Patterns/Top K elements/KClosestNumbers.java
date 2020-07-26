f33164b79c669c1d264b59ba7e739799d0efb569
kinny <ad.sam007@gmail.com>
2020-07-26 12:25:47 -0400

    K Closest numbers

--- Patterns/Top K elements/KClosestNumbers.java
+++ Patterns/Top K elements/KClosestNumbers.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -55,6 +56,29 @@ class KClosestNumbers {
             return low - 1;
         }
         return low;
+    }
+    
+    // two pointers
+    public static List<Integer> findClosestElements2(int[] arr, int K, Integer X) {
+        List<Integer> result = new LinkedList<>();
+        int index = binarySearch(arr, X);
+        int leftPointer = index;
+        int rightPointer = index + 1;
+        for (int i = 0; i < K; i++) {
+            if (leftPointer >= 0 && rightPointer < arr.length) {
+                int diff1 = Math.abs(X - arr[leftPointer]);
+                int diff2 = Math.abs(X - arr[rightPointer]);
+                if (diff1 <= diff2)
+                result.add(0, arr[leftPointer--]); // append in the beginning
+                else
+                result.add(arr[rightPointer++]); // append at the end
+            } else if (leftPointer >= 0) {
+                result.add(0, arr[leftPointer--]);
+            } else if (rightPointer < arr.length) {
+                result.add(arr[rightPointer++]);
+            }
+        }
+        return result;
     }
     public static void main(String[] args) {
         List<Integer> result = KClosestNumbers.findClosestElements(new int[] { 5, 6, 7, 8, 9 }, 3, 7);
