fc881592dcf3f17b148b93508fa64809ae38ac46
Arjun Dass <ad.sam007@gmail.com>
2020-07-30 23:00:58 -0400

    search in a rotated array

--- Practice/SearchInARotatedArray.java
+++ Practice/SearchInARotatedArray.java
@@ -1,36 +1,31 @@
 class SearchInARotatedArray {
     public static int binarySearchRotated(int[] arr, int key) {
         int start = 0;
-        int mid = 0;
         int end = arr.length - 1;
-
-        if (start > end) {
-            return -1;
-        }
-
+        
         while (start <= end) {
-            mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
             
             if (arr[mid] == key) {
                 return mid;
             }
-
-            if (arr[start] <= arr[mid] && key <= arr[mid] && key >= arr[start]) {
-                end = mid - 1;
-            } else if (arr[mid] <= arr[end] && key >= arr[mid] && key <= arr[end]) {
-                start = mid + 1;
-            } else if (arr[start] <= arr[mid] && arr[mid]<= arr[end] && key > arr[mid]) {
-                start = mid + 1;
-            } else if (arr[end] <= arr[mid]) {
-                start = mid + 1;
-            } else if (arr[start] >= arr[mid]) {
-                end = mid - 1;
+            
+            if (arr[start] <= arr[mid]) {
+                if (key >= arr[start] && key < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid +  1;
+                }
             } else {
-                return 1;
+                if (key > arr[mid] && key <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
-
-        return -1;
+        
+         return -1;
     }
 
     public static void main(String[] args) {
