03afdc80f0dd178a41e7b3ba00ae6fa552d9a289
Arjun Dass <ad.sam007@gmail.com>
2020-08-02 17:32:37 -0400

    3 sum

--- Problems/Leetcode/Medium/3Sum.java
+++ Problems/Leetcode/Medium/3Sum.java
@@ -1,27 +1,40 @@
 class 3Sum {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i + 2 < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for (int i=0; i<nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
-            int j = i + 1, k = nums.length - 1;  
-            int target = -nums[i];
-            while (j < k) {
-                if (nums[j] + nums[k] == target) {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
-                    while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
-                } else if (nums[j] + nums[k] > target) {
-                    k--;
-                } else {
-                    j++;
+            
+            searchPair(nums, -nums[i], i+1, result);
+        }
+        
+        return result;
+    }
+    
+    private void searchPair(int[] arr, int targetSum, int left, List<List<Integer>> result) {
+        int right = arr.length - 1;
+        while (left < right) {
+            int currentSum = arr[left] + arr[right];
+            if (currentSum == targetSum) {
+                result.add(Arrays.asList(-targetSum, arr[left], arr[right]));
+                left++;
+                right--;
+                
+                while (left < right && arr[left] == arr[left - 1]) {
+                    left++;
                 }
+                
+                while (left<right && arr[right] == arr[right + 1]) {
+                    right--;
+                }
+            } else if (targetSum > currentSum) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return res;
     }
 }
