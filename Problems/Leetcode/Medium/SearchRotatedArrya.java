f7644f90070121ab7553d6154c156a5ced1cac43
Arjun Dass <ad.sam007@gmail.com>
2020-08-03 11:08:09 -0400

    search in a rotated array

--- Problems/Leetcode/Medium/SearchRotatedArrya.java
+++ Problems/Leetcode/Medium/SearchRotatedArrya.java
@@ -1,37 +1,30 @@
 class Solution {
-    public int search(int[] nums, int target) {
-          return binarySearch(nums, 0,  nums.length - 1, target);
-    }
-    
-    public int binarySearch(int[] arr, int start, int end, int key) {
-        // assuming all the keys are unique.
-
-        if (start > end) {
-          return -1;
-        }
-
-        int mid = start + (end - start) / 2;
-
-        if (arr[mid] == key) {
-           return mid;
-        }
-
-        if (arr[start] <= arr[mid] && key <= arr[mid] && key >= arr[start]) {
-          return binarySearch(arr, start, mid - 1, key);
-        }
-
-        else if (arr[mid] <= arr[end] && key >= arr[mid] && key <= arr[end]) {
-          return binarySearch(arr, mid + 1, end, key);
-        }
-
-        else if (arr[end] <= arr[mid]) {
-          return binarySearch(arr, mid + 1, end, key);
-        }
-
-        else if (arr[start] >= arr[mid]) {
-          return binarySearch(arr, start, mid - 1, key);
-        }
-
-        return -1;
-    }
+  public int search(int[] arr, int key) {
+      int start = 0;
+      int end = arr.length - 1;
+      
+      while(start <= end) {
+          int mid = start + (end - start)/ 2;
+          
+          if (arr[mid] == key) {
+              return mid;
+          }
+          
+          if (arr[start] <= arr[mid]) {
+              if (key >= arr[start] && key < arr[mid]) {
+                  end = mid - 1;
+              } else {
+                  start = mid + 1;
+              }
+          } else {
+              if (key > arr[mid] && key <= arr[end]) {
+                  start = mid + 1;
+              } else {
+                  end = mid - 1;
+              }
+          }
+      }
+      
+      return -1;
+  }
 }
