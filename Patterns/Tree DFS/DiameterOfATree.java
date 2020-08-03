b7db33018c1d71cce562e49807375e435c3c83e5
Arjun Dass <ad.sam007@gmail.com>
2020-08-02 20:57:18 -0400

    binary tree path sum

--- Patterns/Tree DFS/DiameterOfATree.java
+++ Patterns/Tree DFS/DiameterOfATree.java
@@ -1,3 +1,4 @@
+// The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
 import java.util.*;
 
 class TreeNode {
